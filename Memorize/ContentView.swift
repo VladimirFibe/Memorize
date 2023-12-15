c9de1c4d0e8a33f7b6645143fde1b92462685ffb
Vladimir Fibe <motiw@icloud.com>
2023-12-15 14:35:30 +0600

    remove buttons

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,52 +10,17 @@ struct ContentView: View {
         "🚂", "✈️", "🚀", "🛸",
         "🚁", "🛶", "⛵️", "🚤",
         "🛳️", "🚈", "🚊", "🚠"]
-    @State var emojiCount = 20
 
     var body: some View {
-        VStack {
-            cards
-
-            cardCountAdjusters
-        }
-        .padding()
-    }
-
-    var cards: some View {
         ScrollView(.vertical) {
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 70), spacing: 10)], spacing: 10) {
-                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                    CardView(content: emoji)
+                ForEach(emojis.indices, id: \.self) { index in
+                    CardView(content: emojis[index])
                 }
             }
-            .foregroundColor(.accent)
+            .foregroundColor(.red)
         }
-    }
-
-    var cardCountAdjusters: some View {
-        HStack {
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-    }
-
-    var cardAdder: some View {
-        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
-    }
-    
-    var cardRemover: some View {
-        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
-    }
-
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button {
-            emojiCount += offset
-        } label: {
-            Image(systemName: symbol)
-        }
-        .disabled(emojiCount + offset < 1 || emojiCount + offset > emojis.count)
+        .padding()
     }
 }
 
